2a731a0a8fa5f4e973e7b6006dadcbdc4998e9b0
muhammad hanif hajjatul akbar <muhanif37@gmail.com>
2024-07-23 08:55:16 +0700

    update footer and header pusaka

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// NavbarScroll
 const navScroll = document.querySelector(".navbar");
 
 window.addEventListener("scroll", () => {
@@ -8,6 +9,7 @@ window.addEventListener("scroll", () => {
   }
 });
 
+// DataTables
 $(document).ready(function () {
   $("#ikuDaerah").DataTable({
     responsive: true,
@@ -25,4 +27,52 @@ $(document).ready(function () {
     responsive: true,
     paging: false,
   });
+  $("#monografiDesa").DataTable({
+    responsive: true,
+    paging: false,
+  });
 });
+
+// Chart JS
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+// $(document).ready(function () {
+//   var rowCount = $("#indikatorDpa tbody tr").length;
+
+//   // Tentukan batas jumlah data untuk menampilkan pagination
+//   var paginationThreshold = 10;
+
+//   // Inisialisasi DataTables dengan atau tanpa pagination
+//   if (rowCount > paginationThreshold) {
+//     $("#indikatorDpa").DataTable({
+//       paging: true,
+//       pageLength: 10,
+//       lengthMenu: [10, 25, 50, 75, 100],
+//     });
+//   } else {
+//     $("#indikatorDpa").DataTable({
+//       paging: false,
+//     });
+//   }
+// });
